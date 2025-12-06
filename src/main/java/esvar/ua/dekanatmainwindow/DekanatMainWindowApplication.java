b21d143dbce016cd0ba530b6414ec966baffc739
DarkPoul <poulpotter1@gmail.com>
2025-12-06 02:39:01 +0200

    initial

---
+++ src/main/java/esvar/ua/dekanatmainwindow/DekanatMainWindowApplication.java
@@ -0,0 +1,13 @@
+package esvar.ua.dekanatmainwindow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekanatMainWindowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DekanatMainWindowApplication.class, args);
+    }
+
+}
